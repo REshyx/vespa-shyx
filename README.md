037eb718519483e5dad8cae8643ff39f1e857f81
Charles Gueunet <charles.gueunet@gmail.com>
2021-12-15 11:16:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Brief
+
+This project contains VTK filters that uses CGAL for mesh processing.
+
+This project is distributed under a BSD-3 License, but as it is linked to
+CGAL, any binary generated with it retains the GPLv3 license.
---
+++ VTKCGAL/PolygonMeshProcessing/vtkCGALisotropicRemesh.h
@@ -0,0 +1,18 @@
+/**
+ * @class   vtkCGALisotropicRemesh
+ * @brief   remesh using the CGAL isotropic remeshing
+ *
+ * vtkCGALisotropicRemesh is a filter ...
+ */
+
+#ifndef vtkCGALisotropicRemesh_h
+#define vtkCGALisotropicRemesh_h
+
+#include "vtkDataSetAlgorithm.h"
+#include "vtkVTKCGALCoreModule.h" // For export macro
+
+class VTKFILTERSCORE_EXPORT vtkElevationFilter : public vtkDataSetAlgorithm
+{
+}
+
+#endif
