aec3cc7131b88cf580efa67457b87e3bff78b93a
Tiffany Chhim <tiffany.chhim@kitware.com>
2022-04-04 11:35:04 +0200

    Add mesh subdivision filter
    Based on CGAL::Subdivision_method_3.

---
+++ vespa/PolygonMeshProcessing/Testing/TestPMPSubdivisionExecution.cxx
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include "vtkNew.h"
+#include "vtkSphereSource.h"
+#include "vtkTestUtilities.h"
+#include "vtkXMLPolyDataWriter.h"
+
+#include "vtkCGALMeshSubdivision.h"
+
+int TestPMPSubdivisionExecution(int, char* argv[])
+{
+  vtkNew<vtkSphereSource> sphere;
+  sphere->SetRadius(2.0);
+  sphere->SetThetaResolution(8);
+  sphere->SetPhiResolution(8);
+
+  // Create subdivision filter
+  vtkNew<vtkCGALMeshSubdivision> subdivider;
+  subdivider->SetInputConnection(sphere->GetOutputPort());
+  subdivider->SetUpdateAttributes(false);
+
+  vtkNew<vtkXMLPolyDataWriter> writer;
+  writer->SetInputConnection(subdivider->GetOutputPort());
+  writer->SetFileName("sphere_subdivision_sqrt3.vtp");
+  writer->Write();
+
+  // Change to Catmull-Clark method
+  subdivider->SetSubdivisionType(vtkCGALMeshSubdivision::CATMULL_CLARK);
+  writer->SetFileName("sphere_subdivision_catmull_clark.vtp");
+  writer->Write();
+
+  // Change to Loop method
+  subdivider->SetSubdivisionType(vtkCGALMeshSubdivision::LOOP);
+  writer->SetFileName("sphere_subdivision_loop.vtp");
+  writer->Write();
+
+  // Change to Doo-Sabin method
+  subdivider->SetSubdivisionType(vtkCGALMeshSubdivision::DOO_SABIN);
+  writer->SetFileName("sphere_subdivision_doo_sabin.vtp");
+  writer->Write();
+
+  return 0;
+}
---
+++ vespa/PolygonMeshProcessing/vtkCGALMeshSubdivision.cxx
@@ -0,0 +1,119 @@
+#include "vtkCGALMeshSubdivision.h"
+
+// VTK related includes
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkNew.h"
+#include "vtkObjectFactory.h"
+#include "vtkTriangleFilter.h"
+
+// CGAL related includes
+#include <CGAL/subdivision_method_3.h>
+
+vtkStandardNewMacro(vtkCGALMeshSubdivision);
+
+//------------------------------------------------------------------------------
+void vtkCGALMeshSubdivision::PrintSelf(ostream& os, vtkIndent indent)
+{
+  switch (this->SubdivisionType)
+  {
+    case vtkCGALMeshSubdivision::CATMULL_CLARK:
+      os << indent << "SubdivisionType: "
+         << "Catmull-Clark" << std::endl;
+      break;
+    case vtkCGALMeshSubdivision::LOOP:
+      os << indent << "SubdivisionType: "
+         << "Loop" << std::endl;
+      break;
+    case vtkCGALMeshSubdivision::DOO_SABIN:
+      os << indent << "SubdivisionType: "
+         << "Doo-Sabin" << std::endl;
+      break;
+    case vtkCGALMeshSubdivision::SQRT3:
+      os << indent << "SubdivisionType: "
+         << "Sqrt3" << std::endl;
+      break;
+    default:
+      os << indent << "SubdivisionType: "
+         << "Unknown" << std::endl;
+      break;
+  }
+
+  os << indent << "Iterations :" << this->Iterations << std::endl;
+  this->Superclass::PrintSelf(os, indent);
+}
+
+//------------------------------------------------------------------------------
+int vtkCGALMeshSubdivision::RequestData(
+  vtkInformation*, vtkInformationVector** inputVector, vtkInformationVector* outputVector)
+{
+  // Get the input and output data objects.
+  vtkPolyData* input  = vtkPolyData::GetData(inputVector[0]);
+  vtkPolyData* output = vtkPolyData::GetData(outputVector);
+
+  if (!input || !output)
+  {
+    vtkErrorMacro(<< "Missing input or output!");
+    return 0;
+  }
+
+  // Create the triangle mesh for CGAL
+  // ---------------------------------
+
+  std::unique_ptr<CGAL_Mesh> cgalMesh = this->toCGAL(input);
+
+  // CGAL Processing
+  // ---------------
+
+  try
+  {
+    switch (this->SubdivisionType)
+    {
+      case vtkCGALMeshSubdivision::CATMULL_CLARK:
+        CGAL::Subdivision_method_3::CatmullClark_subdivision(
+          cgalMesh->surface, CGAL::parameters::number_of_iterations(this->Iterations));
+        break;
+      case vtkCGALMeshSubdivision::LOOP:
+        CGAL::Subdivision_method_3::Loop_subdivision(
+          cgalMesh->surface, CGAL::parameters::number_of_iterations(this->Iterations));
+        break;
+      case vtkCGALMeshSubdivision::DOO_SABIN:
+        CGAL::Subdivision_method_3::DooSabin_subdivision(
+          cgalMesh->surface, CGAL::parameters::number_of_iterations(this->Iterations));
+        break;
+      case vtkCGALMeshSubdivision::SQRT3:
+        CGAL::Subdivision_method_3::Sqrt3_subdivision(
+          cgalMesh->surface, CGAL::parameters::number_of_iterations(this->Iterations));
+        break;
+      default:
+        vtkErrorMacro("Unknown subdivision method!");
+        break;
+    }
+  }
+  catch (std::exception& e)
+  {
+    vtkErrorMacro("CGAL Exception: " << e.what());
+    return 0;
+  }
+
+  // VTK Output
+  // ----------
+
+  // Triangulate if needed
+  if (this->SubdivisionType == vtkCGALMeshSubdivision::CATMULL_CLARK ||
+    this->SubdivisionType == vtkCGALMeshSubdivision::DOO_SABIN)
+  {
+    vtkNew<vtkTriangleFilter> triangulator;
+    triangulator->SetInputData(this->toVTK(cgalMesh.get()));
+    triangulator->Update();
+    output->ShallowCopy(triangulator->GetOutput());
+  }
+  else
+  {
+    output->ShallowCopy(this->toVTK(cgalMesh.get()));
+  }
+
+  this->interpolateAttributes(input, output);
+
+  return 1;
+}
